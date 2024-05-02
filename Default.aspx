d879f0e686005bef85244ea36ae8d61989a8c785
Ratnesh Kumar Singh <ratnesh10061999@gmail.com>
2024-05-02 14:29:44 +0530

    Add project files.

---
+++ Default.aspx
@@ -0,0 +1,143 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="EmployeeManagement._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <div>
+        <table class="w-100">
+            <tr>
+                <td id="lblEmpInfo">Employee Information</td>
+            </tr>
+            <tr>
+                <td>&nbsp;</td>
+            </tr>
+        </table>
+    </div>
+    <div>
+
+        <table class="w-100">
+            <tr>
+                <td id="lblEmpCode" style="width: 450px">Employee Code</td>
+                <td style="width: 507px">
+                    <asp:TextBox ID="txtEmpCode" runat="server"></asp:TextBox>
+                </td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td id="lblEmpName" style="height: 24px; width: 450px">Employee Name</td>
+                <td style="height: 24px; width: 507px">
+                    <asp:TextBox ID="txtEmpName" runat="server"></asp:TextBox>
+                </td>
+                <td style="height: 24px"></td>
+            </tr>
+            <tr>
+                <td id="lblDOB" style="width: 450px">DOB</td>
+                <td style="width: 507px">
+                    <asp:TextBox ID="txtDOB" runat="server" TextMode="Date"></asp:TextBox>
+                </td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td id="lblGender" style="width: 450px">Gender</td>
+                <td style="width: 507px">
+                    <asp:RadioButtonList ID="rdoGender" runat="server">
+                        <asp:ListItem Value="1">Male</asp:ListItem>
+                        <asp:ListItem Value="0">Female</asp:ListItem>
+                    </asp:RadioButtonList>
+                </td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td id="lblDepart" style="height: 24px; width: 450px">Department</td>
+                <td style="height: 24px; width: 507px">
+                    <asp:TextBox ID="txtDeparment" runat="server"></asp:TextBox>
+                </td>
+                <td style="height: 24px"></td>
+            </tr>
+            <tr>
+                <td id="lblDesignation" style="width: 450px">Designation</td>
+                <td style="width: 507px">
+                    <asp:TextBox ID="txtDesignation" runat="server"></asp:TextBox>
+                </td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td id="lblBS" style="width: 450px">Basic Salary </td>
+                <td style="width: 507px">
+                    <asp:TextBox ID="txtBasicSalary" runat="server"></asp:TextBox>
+                </td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td style="width: 450px">&nbsp;</td>
+                <td style="width: 507px">&nbsp;</td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td style="width: 450px">&nbsp;</td>
+                <td style="width: 507px">
+                    <asp:Button ID="btnEnter" runat="server" Text="Enter" OnClick="btnEnter_Click" />
+                    <asp:Button ID="btnEdit" runat="server" Text="Edit" OnClick="btnEdit_Click" />
+                </td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td style="width: 450px">&nbsp;</td>
+                <td style="width: 507px">&nbsp;</td>
+                <td>&nbsp;</td>
+            </tr>
+        </table>
+
+    </div>
+    <div>
+        <table class="w-100">
+            <tr>
+                <td>
+                    <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" DataSourceID="SqlDataSource1" OnSelectedIndexChanged="GridView_SelectIndexChanged" DataKeyNames="EmployeeCode" OnRowDataBound="GridView1_RowDataBound">
+                        <Columns>
+                            <asp:CommandField ShowSelectButton="True" />
+                            <asp:BoundField DataField="EmployeeCode" HeaderText="EmployeeCode" SortExpression="EmployeeCode" />
+                            <asp:BoundField DataField="EmployeeName" HeaderText="EmployeeName" SortExpression="EmployeeName" />
+                            <asp:BoundField DataField="DateOfBirth" HeaderText="DateOfBirth" SortExpression="DateOfBirth" DataFormatString="{0:MM/dd/yyyy}" />
+                            <%--<asp:CheckBoxField DataField="Gender" HeaderText="Gender" SortExpression="Gender" />--%>
+                            <asp:TemplateField HeaderText="Gender" SortExpression="Gender">
+                                <ItemTemplate>
+                                    <%-- <asp:Label ID="lblGender" runat="server" Text='<%# Eval("Gender") == true ? "Male" : "Female" %>'></asp:Label>--%>
+                                    <asp:Label ID="Label1" runat="server" Text='<%# Eval("Gender") != DBNull.Value && Convert.ToBoolean(Eval("Gender")) ? "Male" : "Female" %>'></asp:Label>
+
+                                </ItemTemplate>
+                            </asp:TemplateField>
+                            <asp:BoundField DataField="Department" HeaderText="Department" SortExpression="Department" />
+                            <asp:BoundField DataField="Designation" HeaderText="Designation" SortExpression="Designation" />
+                            <asp:BoundField DataField="BasicSalary" HeaderText="BasicSalary" SortExpression="BasicSalary" />
+                            <asp:TemplateField HeaderText="DearnessAllowance">
+                                <ItemTemplate>
+                                    <asp:Label ID="lblDearnessAllowance" runat="server"></asp:Label>
+                                </ItemTemplate>
+                            </asp:TemplateField>
+                            <asp:TemplateField HeaderText="ConveyanceAllowance">
+                                <ItemTemplate>
+                                    <asp:Label ID="lblConveyanceAllowance" runat="server"></asp:Label>
+                                </ItemTemplate>
+                            </asp:TemplateField>
+                            <asp:TemplateField HeaderText="HouseRentAllowance">
+                                <ItemTemplate>
+                                    <asp:Label ID="lblHouseRentAllowance" runat="server"></asp:Label>
+                                </ItemTemplate>
+                            </asp:TemplateField>
+                            <asp:TemplateField HeaderText="TotalSalary">
+                                <ItemTemplate>
+                                    <asp:Label ID="lblTotalSalary" runat="server"></asp:Label>
+                                </ItemTemplate>
+                            </asp:TemplateField>
+                        </Columns>
+                    </asp:GridView>
+                    <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:EmployeeInfoConnectionString %>" ProviderName="<%$ ConnectionStrings:EmployeeInfoConnectionString.ProviderName %>" SelectCommand="SELECT * FROM [Employee]"></asp:SqlDataSource>
+                </td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+            </tr>
+        </table>
+    </div>
+</asp:Content>
---
+++ Default.aspx.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+using System.Security.AccessControl;
+
+namespace EmployeeManagement
+{
+    public partial class _Default : Page
+    {
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                // This block of code will only execute when the page is initially loaded
+                // It will not execute on postback events like button clicks
+                GetEmployeeList();
+            }
+        }
+
+        protected void GridView_SelectIndexChanged(object sender, EventArgs e)
+        {
+            // Check if a row is selected
+            if (GridView1.SelectedRow != null)
+            {
+                // Populate form controls with selected row data
+                txtEmpCode.Text = GridView1.SelectedRow.Cells[1].Text;
+                txtEmpName.Text = GridView1.SelectedRow.Cells[2].Text;
+                txtDOB.Text = GridView1.SelectedRow.Cells[3].Text;
+                // Access the Label control in the selected row
+                Label lblGender = (Label)GridView1.SelectedRow.FindControl("Label1");
+                if (lblGender != null)
+                {
+                    string genderText = lblGender.Text;
+                    if (genderText.Equals("Male", StringComparison.OrdinalIgnoreCase))
+                    {
+                        rdoGender.SelectedValue = "1"; // Male
+                    }
+                    else
+                    {
+                        rdoGender.SelectedValue = "0"; // Female
+                    }
+                }
+                txtDeparment.Text = GridView1.SelectedRow.Cells[5].Text;
+                txtDesignation.Text = GridView1.SelectedRow.Cells[6].Text;
+                txtBasicSalary.Text = GridView1.SelectedRow.Cells[7].Text;
+            }
+        }
+
+        protected void btnEnter_Click(object sender, EventArgs e)
+        {
+            int empCode = int.Parse(txtEmpCode.Text);
+            string empName = txtEmpName.Text;
+            string empDOB = txtDOB.Text;
+            int genderValue = 1; // Default to Male
+            int.TryParse(rdoGender.SelectedValue, out genderValue);
+            string department = txtDeparment.Text;
+            string designation = txtDesignation.Text;
+            string basicSalary = txtBasicSalary.Text;
+
+            using (SqlConnection connection = new SqlConnection("Data Source=LAPTOP-MJVV7V1E; Initial Catalog =EmployeeInfo; Persist Security Info=True;User ID = sa; Password=Ratnesh@1999;Encrypt=False"))
+            {
+                connection.Open();
+                string query = "INSERT INTO Employee (EmployeeCode, EmployeeName, DateOfBirth, Gender, Department, Designation, BasicSalary) " +
+                               "VALUES (@EmployeeCode, @EmployeeName, @DateOfBirth, @Gender, @Department, @Designation, @BasicSalary)";
+                SqlCommand cmd = new SqlCommand(query, connection);
+                cmd.Parameters.AddWithValue("@EmployeeCode", empCode);
+                cmd.Parameters.AddWithValue("@EmployeeName", empName);
+                cmd.Parameters.AddWithValue("@DateOfBirth", empDOB);
+                cmd.Parameters.AddWithValue("@Gender", genderValue);
+                cmd.Parameters.AddWithValue("@Department", department);
+                cmd.Parameters.AddWithValue("@Designation", designation);
+                cmd.Parameters.AddWithValue("@BasicSalary", basicSalary);
+                cmd.ExecuteNonQuery();
+
+                ScriptManager.RegisterStartupScript(this, this.GetType(), "script", "alert('Successfully saved.');", true);
+                GetEmployeeList();
+            }
+        }
+
+        void GetEmployeeList()
+        {
+            using (SqlConnection connection = new SqlConnection("Data Source =LAPTOP-MJVV7V1E; Initial Catalog = EmployeeInfo; Persist Security Info=True;User ID = sa; Password=Ratnesh@1999;Encrypt=False"))
+            {
+                connection.Open();
+                string query = "SELECT * FROM Employee";
+                SqlCommand cmd = new SqlCommand(query, connection);
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                sqlDataAdapter.Fill(dt);
+                GridView1.DataBind();
+            }
+        }
+
+        protected void btnEdit_Click(object sender, EventArgs e)
+        {
+            int empCode = int.Parse(txtEmpCode.Text);
+            string empName = txtEmpName.Text;
+            string empDOB = txtDOB.Text;
+            int genderValue = 1; // Default to Male
+            int.TryParse(rdoGender.SelectedValue, out genderValue);
+            string department = txtDeparment.Text;
+            string designation = txtDesignation.Text;
+            string basicSalary = txtBasicSalary.Text;
+
+            using (SqlConnection connection = new SqlConnection("Data Source=LAPTOP-MJVV7V1E; Initial Catalog=EmployeeInfo; Persist Security Info=True;User ID=sa; Password=Ratnesh@1999;Encrypt=False"))
+            {
+                connection.Open();
+                string query = "UPDATE Employee SET EmployeeName = @EmployeeName, DateOfBirth = @DateOfBirth, Gender = @Gender, Department = @Department, Designation = @Designation, BasicSalary = @BasicSalary WHERE EmployeeCode = @EmployeeCode";
+                SqlCommand cmd = new SqlCommand(query, connection);
+                cmd.Parameters.AddWithValue("@EmployeeCode", empCode);
+                cmd.Parameters.AddWithValue("@EmployeeName", empName);
+                cmd.Parameters.AddWithValue("@DateOfBirth", empDOB);
+                cmd.Parameters.AddWithValue("@Gender", genderValue);
+                cmd.Parameters.AddWithValue("@Department", department);
+                cmd.Parameters.AddWithValue("@Designation", designation);
+                cmd.Parameters.AddWithValue("@BasicSalary", basicSalary);
+                cmd.ExecuteNonQuery();
+
+                ScriptManager.RegisterStartupScript(this, this.GetType(), "script", "alert('Record updated successfully.');", true);
+                GetEmployeeList();
+            }
+        }
+
+        protected void GridView1_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            if (e.Row.RowType == DataControlRowType.DataRow)
+            {
+                // Calculate DearnessAllowance
+                // DearnessAllowance = BasicSalary * 40%
+                double basicSalary = double.Parse(e.Row.Cells[7].Text);
+                double dearNessAllowance = basicSalary * 0.4;
+
+                // Calculate ConveyanceAllowance
+                // ConveyanceAllowance = DearnessAllowance * 10% or 250 (which ever is lower)
+                double conveyanceAllowance = Math.Min(dearNessAllowance * 0.1, 250);
+
+                // Calculate HouseRentAllowance
+                // HouseRentAllowance = BasicSalary * 25%   or 1500 (which ever is higher)
+                double houseRentAllowance = Math.Max(basicSalary * 0.25, 1500);
+
+                // Calculate GrossSalary
+                // GrossSalary = BasicSalary + DearnessAllowance + ConveyanceAllowance + HouseRentAllowance (Do not display Gross Salary)
+                double grossSalary = basicSalary + dearNessAllowance + conveyanceAllowance + houseRentAllowance;
+
+                // Calculate PT
+                // Deductions : PT = (GrossSalary <= 3000 then 100, GrossSalary > 3000 and GrossSalary<= 6000 then 150 else 200)
+                double pt = (grossSalary <= 3000) ? 100 : ((grossSalary > 3000 && grossSalary <= 6000) ? 150 : 200);
+
+                // Calculate TotalSalary
+                // TotalSalary = BasicSalary + DearnessAllowance + ConveyanceAllowance + HouseRentAllowance - PT or GrossSalary - PT
+                double totalSalary = grossSalary - pt;
+
+                // Set the calculated values to the labels in the GridView
+                ((Label)e.Row.FindControl("lblDearnessAllowance")).Text = dearNessAllowance.ToString();
+                ((Label)e.Row.FindControl("lblConveyanceAllowance")).Text = conveyanceAllowance.ToString();
+                ((Label)e.Row.FindControl("lblHouseRentAllowance")).Text = houseRentAllowance.ToString();
+                ((Label)e.Row.FindControl("lblTotalSalary")).Text = totalSalary.ToString();
+            }
+        }
+
+    }
+}
